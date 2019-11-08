849b1570836d5689f738f73c2892cd04b104ab93
Han-Seungwoo <57412986+Han-Seungwoo@users.noreply.github.com>
2019-11-08 13:46:42 +0100

    Update han.py

--- han.py
+++ han.py
@@ -1,3 +1,22 @@
 import truecase
+from bpe import Encoder
+from tokenizer import tokenize, TOK
+
+def make_truecase(text):
+  for s in text:
+    truecase = truecase.get_true_case(s)
+  return truecase
+
+
+def make_bpe(text):
+  encoder = Encoder(200, pct_bpe=0.88)
+  bpe = encoder.fit(text.split('\n'))
+  return bpe
+
+def make_token(text):
+  for s in text:
+    token = tekenize(s)
+  return token
+
 
 
